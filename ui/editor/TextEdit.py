7d3331ea2c72c13434c1cdd3179d6fe155b83578
Nick Wilde <Nick Wilde>
2017-04-18 23:48:16 -0600

    add undo redo functionality for TextEdit

--- ui/editor/TextEdit.py
+++ ui/editor/TextEdit.py
@@ -4,3 +4,7 @@ class TextEdit(QTextEdit):
 
     def __init__(self, text="", parent=None):
         super(TextEdit, self).__init__(text, parent)
+        self.setup()
+
+    def setup(self):
+        self.setUndoRedoEnabled(True)
